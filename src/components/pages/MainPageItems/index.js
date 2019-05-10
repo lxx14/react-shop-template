1fc3992818516cb36ca4c77d14dc29a06e561d7e
lxx14 <lxx1427@gmail.com>
2019-05-10 16:57:39 +0300

    add single page item

--- src/components/pages/MainPageItems/index.js
+++ src/components/pages/MainPageItems/index.js
@@ -4,6 +4,10 @@ import { NavLink } from 'react-router-dom';
 import './style.scss';
 
 class MainPageItems extends Component {
+  constructor(props) {
+    super(props)
+  }
+  
   render() {
     const renderItems = this.props.items.map(item=><div key={item.id} className='img-wrap'><NavLink to={`/singleItem/${item.id}`}>Go to item</NavLink><img src={item.icon.src} /></div>)
     return(
--- src/components/pages/SingleItem/index.js
+++ src/components/pages/SingleItem/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './style.scss';
 
-const SingleItem = () => {
+const SingleItem = (props) => {
+  const {name, price, category, stocked, icon} = props.oneItem;
   return(
-    <div className='page'>
-      <h1>SingleItem</h1>
+    <div className='page single-item-page'>
+      <h1>{name}</h1>
+      <p>price: {price}</p>
+      <p>{stocked ? 'STOCKED' : 'NOT STOCKED'}</p>
+      <img src={icon.src} alt={name} />
       <NavLink to='/items' className='back'><i className="far fa-arrow-alt-circle-left"></i></NavLink>
     </div>
   )
 }
 
-export default SingleItem;
+
+const mapStateToProps = (state, ownProps) => {
+  const singleId = ownProps.match.params.id;
+  const oneItem = state.itemsContainer.items.find( item => parseFloat(singleId) === item.id);
+  console.log(oneItem);
+  return {
+    oneItem
+  }
+}
+
+export default connect(mapStateToProps)(SingleItem);
