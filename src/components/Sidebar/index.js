e58622cd1edc92946b4729939cfd6ed2b2a603c5
lxx14 <lxx1427@gmail.com>
2019-05-31 15:34:21 +0300

    fix bugs

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,9 +3,11 @@ import { connect } from 'react-redux';
 import './style.scss';
 
 const Sidebar = (props) => {
-  console.log(props.sideBarItem);
-  const {id, name, src} = props.sideBarItem;
-  const renderFavorite=props.sideBarItem.map(item=><div key={item.id}><img src={item.icon.src} alt={item.name} /></div>)
+  if (props.sideBarItems.length>0) {
+  const renderFavorite=props.sideBarItems.map(item=><div key={item.id}><img src={item.icon.src} alt={item.name} /></div>)
+  } else {
+    const renderFavorite=<p>empty now!</p>
+  }
   return(
     <div className='sidebar'>
       <h1>Sidebar</h1>
@@ -16,7 +18,7 @@ const Sidebar = (props) => {
 }
 
 const mapStateToProps = (state) => ({
-  sideBarItem: state.sidebar.favorite
+  sideBarItems: state.sidebar.favorite
 })
 
 export default connect(mapStateToProps)(Sidebar);
