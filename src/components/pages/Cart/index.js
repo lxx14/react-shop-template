496a0246d77675a18ee80feff9c7ca40e6f8cb6d
lxx14 <lxx1427@gmail.com>
2019-05-31 21:08:27 +0300

    create deleted from cart

--- src/components/pages/Cart/index.js
+++ src/components/pages/Cart/index.js
@@ -5,10 +5,11 @@ import './style.scss';
 const Cart = (props) => {
   const deleteFromCart = (e) => {
     props.deleteCart(e.target.id);
+    console.log(e.target.id)
   }
 
-  const cartRender = props.cart.map(item=><div className="cart-item" key={item.id}><div><img src={item.icon.src} alt={item.name} /><p><button className="button-ok" type="button" onClick={()=>alert('Thanks for purchase! :)')}>confirm</button><button type="button" className="button-delete" onClick={deleteFromCart} id={item.id}>delete</button></p></div><p className="price">{item.price}</p></div>)
-  return(
+  const cartRender = props.cart.map(item => <div className="cart-item" key={item.id}><div><img src={item.icon.src} alt={item.name} /><p><button className="button-ok" type="button" onClick={() => alert('Thanks for purchase! :)')}>confirm</button><button type="button" className="button-delete" onClick={deleteFromCart} id={item.id}>delete</button></p></div><p className="price">{item.price}</p></div>)
+  return (
     <div className='page cart'>
       <h1>Cart</h1>
       {cartRender}
@@ -21,7 +22,7 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  deleteCart: (data) => ({type: "DELETE_FROM_CART", data})
+  deleteCart: (data) => dispatch({ type: "DELETE_FROM_CART", data })
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- src/components/pages/Cart/reducer.js
+++ src/components/pages/Cart/reducer.js
@@ -2,13 +2,19 @@ const initialState = {
   cart: [],
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
-    case "ADD_TO_CART": 
-    return {
-      ...state,
-      cart: [...state.cart, action.data]
-    }
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cart: [...state.cart, action.data]
+      }
+    case "DELETE_FROM_CART":
+    console.log(action.data);
+      return {
+        ...state,
+        cart: state.cart.filter(item=>item.id !== parseFloat(action.data))
+      }
   }
   return state;
 };
