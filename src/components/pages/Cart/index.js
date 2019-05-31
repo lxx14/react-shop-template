41d05a003c64f675fc61e07a47e9ab69b6e4d889
lxx14 <lxx1427@gmail.com>
2019-05-31 21:02:02 +0300

    add button delete from cart

--- src/components/pages/Cart/index.js
+++ src/components/pages/Cart/index.js
@@ -3,7 +3,11 @@ import { connect } from 'react-redux';
 import './style.scss';
 
 const Cart = (props) => {
-  const cartRender = props.cart.map(item=><div className="cart-item" key={item.id}><div><img src={item.icon.src} alt={item.name} /><p><button className="button-ok" type="button" onClick={()=>alert('Thanks for purchase! :)')}>confirm</button><button type="button" className="button-delete">delete</button></p></div><p className="price">{item.price}</p></div>)
+  const deleteFromCart = (e) => {
+    props.deleteCart(e.target.id);
+  }
+
+  const cartRender = props.cart.map(item=><div className="cart-item" key={item.id}><div><img src={item.icon.src} alt={item.name} /><p><button className="button-ok" type="button" onClick={()=>alert('Thanks for purchase! :)')}>confirm</button><button type="button" className="button-delete" onClick={deleteFromCart} id={item.id}>delete</button></p></div><p className="price">{item.price}</p></div>)
   return(
     <div className='page cart'>
       <h1>Cart</h1>
@@ -16,4 +20,8 @@ const mapStateToProps = (state) => ({
   cart: state.cartContainer.cart
 })
 
-export default connect(mapStateToProps)(Cart);
+const mapDispatchToProps = (dispatch) => ({
+  deleteCart: (data) => ({type: "DELETE_FROM_CART", data})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
